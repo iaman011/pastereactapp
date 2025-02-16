e1dfb4f59798ce5bc6e36e44804014bd4f090871
Aman <iaman.singh011@gmail.com>
2025-02-16 21:20:07 +0530

    init

--- pastereactapp/src/main.jsx
+++ pastereactapp/src/main.jsx
@@ -4,15 +4,18 @@ import './index.css'
 import App from './App.jsx'
 import { store } from './store.js'
 import { Provider } from 'react-redux'
-import { Toaster } from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast'
+import { BrowserRouter } from "react-router-dom"
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-    <App />
-    <Toaster />
+      <BrowserRouter basename="/pastereactapp">
+        <App />
+      </BrowserRouter>
+      <Toaster />
     </Provider>
-  
-  </StrictMode>,
+  </StrictMode>
 );
+
